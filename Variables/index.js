a56faac5a0302735b79c30d5c2b337e6b7382897
tsgold <tsgold@gmail.com>
2018-10-30 18:55:44 +0700

    tambahan file

---
+++ Variables/index.js
@@ -0,0 +1,47 @@
+// //keyword - namaVariable
+// var namaOrang = "Yogie" //old
+
+// //es6
+// const color = "red" //tidak bisa reassign 
+// color = "green" // Error
+
+// let brand = "lenovo" // bisa reassign 
+// brand = "Acer" // hasilnya Acer
+
+// // variable kosong
+// const cars
+// let motors 
+// motors = ["Honda", "Yamaha"]
+
+
+// //Data Types
+// const peopleName= "Yogie" // String // cleancode nama variable specific
+// const peopleAge = 17   // Int atau number
+// const isHandsome = true  //Boolen - true or false isHome (pertanyaan)
+// const ride = {              // Object -- 1 object punya beberapa property 
+//     category: 'motor',
+//     brand: 'yamaha',
+//     type:'Xmax'
+// } // object pakai kurung kurawal
+// const numbers = [1, 3, 5, 7]  //Array -- kurung siku -- untuk menyimpan beberapa object di pisah dengan koma 
+// // array menyimpan kumpulan data
+
+const cars = [
+    {
+        category: "mobil",
+        brand: "honda",
+        type: "crv"
+    } 
+    // {
+    //     category: 'mobil',
+    //     brand: 'honda',
+    //     type: 'crv'
+    // }
+]
+console.log (`my car is ${cars.category}. i am using ${cars.brand}`);
+
+
+
+
+// Template string
+const namaOrang = `Yogie`
---
+++ Variables/sampleOne.js
@@ -0,0 +1,35 @@
+// const peopleName = "Yogie";
+// const peopleAge = 17;
+// const peopleGender = "Male";
+// const peopleAddress = "Jakarta";
+// const peoplePhone = "08129383748"
+// const peopleRide = "Mobil"
+
+// console.log("Hello " + peopleName) //ingin menampilkan hasil sesuatu
+// console.log ("Hello " + peopleName +
+//             ". I am " + peopleAge +
+//             " years old. I live in " + peopleAddress
+//             );
+
+// console.log(`Hello ${peopleName}`)
+// console.log (`hello ${peopleName}. I am ${peopleAge} Years old. I Live in ${peopleAddress}` );
+
+const hero = {
+  heroName: "Spiderman",
+  heroAlias: "Spidey",
+  heroColor: "Red-Blue",
+  heroGender: "Male"
+};
+
+console.log(`${hero.heroName} ${hero.heroGender}`);
+
+/* const shops = {
+  shop_id: 3739573,
+  shop_url: "https://www.tokopedia.com/telkomsel",
+  shop_name: "Telkomsel",
+  logo_url: "https://ecs7.tokopedia.ner/img/cache/244/attachment/2018/9/10",
+  is_new: 0
+};
+
+console.log(`${shops.shop_id} ${shops.shop_name}`);
+ */
